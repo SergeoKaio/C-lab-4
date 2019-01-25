a4a01cc60b163849bf5b488025c118bf5152ed5c
alekseypivovar <you@example.com>
2019-01-25 21:54:15 +0300

    Release

---
+++ src/task1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+int comp(const void *a, const void *b)
+{
+	return strlen(*(char**)a) - strlen(*(char**)b);
+}
+
+void lineSort(char *str[], int size)
+{
+	qsort(str, size, sizeof(char**), comp);
+
+
+}
+
+void printLines(const char *str[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		puts(str[i]);
+	}
+
+
+}
