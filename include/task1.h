8d2dee7cb3d1aa790da23451cfdae4942fa387c4
Nikolai <44969889+nbuzynin@users.noreply.github.com>
2019-02-17 21:57:28 +0300

    Файлы заголовков

---
+++ include/task1.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#define LINES 32						//Количество строк
+#define WORDS 256						//Количество букв в строке
+
+void lineSort(char *str[], int size);
+void printLines(const char *str[], int size);
---
+++ include/task2.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <string.h>
+#define CHARS 256							//Количество символов
+
+char * reverseWords(char * in, char *out);
