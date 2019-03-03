8bce3813e0e7df442ebbab2c158166e81c688eac
Усоян Шамил <shamila2@yandex.ru>
2019-03-03 09:40:26 +0300

    f3

--- src/task3.cpp
+++ src/task3.cpp
@@ -8,7 +8,7 @@ int isPalindrome(char * str)
 	int len = strlen(str);
 	for (int i = 0; i < len / 2; ++i) 
 	{
-		if (str[i] != str[len - i - 2]) 
+		if (str[i] != str[len - i - 1]) 
 			return 0;
 	}
 	return 1;
