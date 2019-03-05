c8d01d412a4c6ed950d4ec954fd02ab9caafc19b
SergeoKaio <44640730+SergeoKaio@users.noreply.github.com>
2019-03-05 13:41:11 +0300

    Add files via upload

---
+++ src/main2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "task2.h"
+
+#define N 128
+
+int main()
+{
+	char in[N] = { 0 }, out[N] = { 0 };
+	printf("Enter a string: \n");
+	fgets(in, N, stdin);
+	puts(reverseWords(in, out));
+	return 0;
+}
